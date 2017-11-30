57607013c1830cdcd4259f976ffb60ce6bfd62b8
Tidhar Klein Orbach <tidhar.o@taboola.com>
2017-11-30 13:06:01 +0200

    DEV-19671: set archive logs to accept service name

--- plugins/getInstalledPluginsHpi.groovy
+++ plugins/getInstalledPluginsHpi.groovy
@@ -6,7 +6,7 @@ import jenkins.model.Jenkins
 def outputDirectory = "/tmp/downloadedPlugins"
 def jenkinsPubliRepoUrl = "https://repo.jenkins-ci.org/public"
 
-Jenkins.instance.getPluginManager().getPlugins().each{
+Jenkins.getActiveInstance().getPluginManager().getPlugins().each{
     attributes = it.manifest.getMainAttributes()
     groupPath = attributes.getValue('Group-Id').replace(".", "/")
     artifactPath = attributes.getValue('Short-Name').replace(".", "/")
